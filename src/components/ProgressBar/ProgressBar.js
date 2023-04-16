bf5b11e872070ba5c1c22ef2c0d2a75ca436e921
Sylvain <s.auguy@gmail.com>
2023-04-16 21:07:17 +0200

    style progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,44 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <>
+    <Wrapper className={size} role='progressbar' aria-labelledby='progress' aria-valuenow={value}>
+      <ProgressBarElement value={value}/>
+    </Wrapper>
+  </>
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  width: 370px;
+  padding: 0;
+
+  &.large {
+    height: 24px;
+    padding: 4px;
+  }
+
+  &.medium {
+    height: 12px;
+  }
+
+  &.small {
+    height: 8px;
+  }
+`;
+
+
+const ProgressBarElement = styled.div`
+  width: ${props => props.value}%;
+  height: 100%;
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+
+  ${props => props.value > 99.6 && `
+    border-radius: 4px ${Math.round(4 - (100 - props.value) * 10)}px ${Math.round(4 - (100 - props.value) * 10)}px 4px
+  `};
+`;
 
 export default ProgressBar;
